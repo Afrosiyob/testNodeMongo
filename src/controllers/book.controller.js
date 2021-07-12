9882c1e5cf953bfd4ca860ea5b71a74b3cb97102
Afrosiyob <45900728+Afrosiyob@users.noreply.github.com>
2021-07-12 17:18:41 +0500

    add
    add

--- src/controllers/book.controller.js
+++ src/controllers/book.controller.js
@@ -1,42 +1,41 @@
-const { BookModel } = require( "../models/book.model" );
+const { BookModel } = require("../models/book.model");
 
-const createBook = async ( req, res ) => {
+const createBook = async(req, res) => {
     const { name } = req.body;
-    const checkName = await BookModel.findOne( { name } );
-    if ( checkName ) {
-        res.status( 400 ).json( { message: "enter other name" } );
+    const checkName = await BookModel.findOne({ name });
+    if (checkName) {
+        res.status(400).json({ message: "enter other name" });
     } else {
-        const newBook = new BookModel( {
+        const newBook = new BookModel({
             name,
             owner: req.user.userId,
-        } );
+        });
         await newBook.save();
-        res.status( 200 ).json( { message: "new book created", data: newBook } );
+        res.status(200).json({ message: "new book created", data: newBook });
     }
 };
 
-const getBooks = async ( req, res ) => {
+const getBooks = async(req, res) => {
     const { pageNumber, pageSize } = req.query;
     //  /api/books?pagenumber=3&pageSize=10
-    const books = await BookModel.find( { owner: req.user.userId } )
-        .select( "name owner" )
-        .populate( "owner", "email -_id" )
-        .skip( ( parseInt( pageNumber ) - 1 ) * parseInt( pageSize ) )
-        .limit( parseInt( pageSize ) )
-    const total
+    const books = await BookModel.find({ owner: req.user.userId })
+        .select("name owner")
+        .populate("owner", "email -_id")
+        .skip((parseInt(pageNumber) - 1) * parseInt(pageSize))
+        .limit(parseInt(pageSize));
 
-    if ( !books ) {
-        res.status( 400 ).json( { message: "nothing" } );
+    if (!books) {
+        res.status(400).json({ message: "nothing" });
     } else {
-        res.status( 200 ).json( { message: "all books", data: books } );
+        res.status(200).json({ message: "all books", data: books });
     }
 };
 
-const getBook = async ( req, res ) => { };
+const getBook = async(req, res) => {};
 
-const updateBook = async ( req, res ) => { };
+const updateBook = async(req, res) => {};
 
-const deleteBook = async ( req, res ) => { };
+const deleteBook = async(req, res) => {};
 
 module.exports = {
     createBook,
